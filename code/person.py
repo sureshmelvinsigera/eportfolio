01fdfca951037f5a36b814cb34f268e01865bf59
Suresh Melvin Sigera <20378525+sureshmelvinsigera@users.noreply.github.com>
2021-05-25 11:24:50 -0400

    Create person.py

---
+++ code/person.py
@@ -0,0 +1,12 @@
+class Person:
+
+	"""Represents a generic Person."""
+
+def __init__(self, first, last, weight, height):
+		self.first_name = first
+		self.last_name = last
+		self.weight_in_lbs = weight
+		self.height_in_inches = height
+
+if __name__=='__main__':
+	print(Person)
